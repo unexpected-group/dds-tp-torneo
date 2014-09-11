d06c182f2f132d77d18fa5ef319f8af2227c89ad
Juan Pablo Jacob <jacobjuampe@hotmail.com>
2014-09-11 13:50:20 -0300

    Agrego que los equipos arranquen ocultos y luego se muestren con el click

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,84 +1,89 @@
 (function() {
-    var app = angular.module('app', []);
+	var app = angular.module('app', []);
 
-    app.controller('JugadoresController', ['$http', function($http) {
+	app.controller('JugadoresController', [ '$http', function($http) {
 
-        var store = this;
-        
-        store.jugadores = [];
+		var store = this;
 
-        $http.get('/jugadores').success(function(jugadores) {
-            store.jugadores = jugadores;
-        });
-    }]);
-    
-    app.controller('GeneralController', ['$http', function($http) {
+		store.jugadores = [];
 
-        var store = this;
-        
-        store.visible = 0;
-        store.nombre = "";
-        store.jugador = {};
+		$http.get('/jugadores').success(function(jugadores) {
+			store.jugadores = jugadores;
+		});
+	} ]);
+
+	app.controller('GeneralController', [ '$http', function($http) {
+
+		var store = this;
+
+		store.visible = 0;
+		store.nombre = "";
+		store.jugador = {};
 
 		store.actualizar = function(n) {
-        	store.nombre = n;
-        	store.visible = 1;
-        	$http.get('/jugadores/' + store.nombre).success(function(jugador) {
-		        store.jugador = jugador;
-		    });
-        };
-        
-        store.ocultar = function() {
-        	store.visible = 0;
-        };
-        
-        store.estaVisible = function() {
-        	return store.visible === 1;
-        };
-    }]);
+			store.nombre = n;
+			store.visible = 1;
+			$http.get('/jugadores/' + store.nombre).success(function(jugador) {
+				store.jugador = jugador;
+			});
+		};
 
-    app.controller('EquipoController', ['$http', function($http) {
+		store.ocultar = function() {
+			store.visible = 0;
+		};
 
-        var store = this;
-        
-        store.jugadores = [];
+		store.estaVisible = function() {
+			return store.visible === 1;
+		};
+	} ]);
 
-        store.generarEquipos = function() {
-            $http.get('/generar-equipos-opciones').success(function(jugadores) {
-                console.log(jugadores);
-                store.jugadores = jugadores;
-            });
-        };
-        
-    }]);
+	app.controller('EquipoController', [ '$http', function($http) {
 
-    app.controller('OrdenamientoController', ['$http', function($http) {
+		var store = this;
 
-        var store = this;
-        
-        store.ordenadores = [];
+		store.visible = 0;
+		store.jugadores = [];
 
-        $http.get('/criterios-ordenamiento').success(function(ordenadores) {
-            store.ordenadores = ordenadores;
-        });
+		store.generarEquipos = function() {
+			$http.get('/generar-equipos-opciones').success(function(jugadores) {
+				store.jugadores = jugadores;
+				store.visible = 1;
+			});
+		};
 
-        store.setOrdenador = function() {
-            $http.get('/criterios-ordenamiento/' + store.seleccionado);  
-        };
-    }]);
+		store.estaVisible = function() {
+			return store.visible === 1;
+		};
 
-    app.controller('ArmadoController', ['$http', function($http) {
+	} ]);
 
-        var store = this;
-        
-        store.armadores = [];
+	app.controller('OrdenamientoController', [ '$http', function($http) {
 
-        $http.get('/criterios-armado').success(function(armadores) {
-            store.armadores = armadores;
-        });
+		var store = this;
 
-        store.setArmador = function() {
-            $http.get('/criterios-armado/' + store.seleccionado);
-        };
-    }]);
+		store.ordenadores = [];
+
+		$http.get('/criterios-ordenamiento').success(function(ordenadores) {
+			store.ordenadores = ordenadores;
+		});
+
+		store.setOrdenador = function() {
+			$http.get('/criterios-ordenamiento/' + store.seleccionado);
+		};
+	} ]);
+
+	app.controller('ArmadoController', [ '$http', function($http) {
+
+		var store = this;
+
+		store.armadores = [];
+
+		$http.get('/criterios-armado').success(function(armadores) {
+			store.armadores = armadores;
+		});
+
+		store.setArmador = function() {
+			$http.get('/criterios-armado/' + store.seleccionado);
+		};
+	} ]);
 })();
