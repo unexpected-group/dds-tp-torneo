b22cbac7bfaa15e7ab49370683d82d7324b7c21d
chanozignego <chano__17@hotmail.com>
2014-10-09 17:04:49 -0300

    Agrego el tipo de cada campo

--- script_creacion.sql
+++ script_creacion.sql
@@ -1,28 +1,28 @@
 CREATE TABLE partido (
-	cod_partido PRIMARY KEY,
-	fecha,
+	cod_partido NUMERIC(10, 0) PRIMARY KEY,
+	fecha DATETIME,
 	estado,
-	cod_config
-	cod_inscripcion,
-	cod_propuesta,
+	cod_config NUMERIC(10, 0),
+	cod_inscripcion NUMERIC(10, 0),
+	cod_propuesta NUMERIC(10, 0),
 	FOREIGN KEY (cod_config) REFERENCES configuracion (cod_config),
 	FOREIGN KEY (cod_inscripcion) REFERENCES inscripcion (cod_inscripcion),
 	FOREIGN KEY (cod_propuesta) REFERENCES propuesta (cod_propuesta)
 );
 
 CREATE TABLE configuracion (
-	cod_config PRIMARY KEY,
-	armador,
-	ordenador
+	cod_config NUMERIC(10, 0) PRIMARY KEY,
+	armador, /* ESTE ES UN STRING?*/
+	ordenador /* ESTE ES UN STRING?*/
 );
 
 CREATE TABLE jugador (
-	cod_jugador PRIMARY KEY,
-	nombre,
+	cod_jugador NUMERIC(10, 0) PRIMARY KEY,
+	nombre NVARCHAR(30),
 	edad,
-	cod_jugador,
-	cod_infraccion,
-	cod_calif,
+	cod_jugador NUMERIC(10, 0),
+	cod_infraccion NUMERIC(10, 0),
+	cod_calif NUMERIC(10, 0),
 	handicap,
 	FOREIGN KEY (cod_jugador) REFERENCES jugador (cod_jugador),
 	FOREIGN KEY (cod_infraccion) REFERENCES infraccion (cod_infraccion),
@@ -30,34 +30,34 @@ CREATE TABLE jugador (
 );
 
 CREATE TABLE calificacion (
-	cod_calif PRIMARY KEY,
+	cod_calif NUMERIC(10, 0) PRIMARY KEY,
 	puntaje,
-	descripcion,
-	cod_jugador,
-	cod_partido,
+	descripcion NVARCHAR(255),
+	cod_jugador NUMERIC(10, 0),
+	cod_partido NUMERIC(10, 0),
 	FOREIGN KEY (cod_jugador) REFERENCES jugador (cod_jugador),
 	FOREIGN KEY (cod_partido) REFERENCES partido (cod_partido)
 );
 
 CREATE TABLE infraccion (
-	cod_infraccion PRIMARY KEY,
-	fecha,
-	motivo
+	cod_infraccion NUMERIC(10, 0) PRIMARY KEY,
+	fecha DATETIME,
+	motivo NVARCHAR(255)
 );
 
 CREATE TABLE propuesta (
-	cod_propuesta PRIMARY KEY,
-	cod_jugador,
-	fecha,
-	fecha_rechazo,
-	motivo_rechazo,
+	cod_propuesta NUMERIC(10, 0) PRIMARY KEY,
+	cod_jugador NUMERIC(10, 0),
+	fecha DATETIME,
+	fecha_rechazo DATETIME,
+	motivo_rechazo NVARCHAR(255),
 	estado,
 	FOREIGN KEY (cod_jugador) REFERENCES jugador (cod_jugador)
 );
 
 CREATE TABLE inscripcion (
-	cod_inscripcion PRIMARY KEY,
-	cod_jugador,
+	cod_inscripcion NUMERIC(10, 0) PRIMARY KEY,
+	cod_jugador NUMERIC(10, 0),
 	tipo,
 	FOREIGN KEY (cod_jugador) REFERENCES jugador (cod_jugador)
 );
