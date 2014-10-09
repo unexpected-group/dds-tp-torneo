c5c49b7463098f50cf5998f480ccd64b58bb1591
chanozignego <chano__17@hotmail.com>
2014-10-09 16:37:08 -0300

    Agrego FKs al script de creacion

--- script_creacion.sql
+++ script_creacion.sql
@@ -5,6 +5,9 @@ CREATE TABLE partido (
 	id_config
 	id_inscripcion,
 	id_propuesta,
+	FOREIGN KEY (id_config) REFERENCES configuracion (id_config),
+	FOREIGN KEY (id_inscripcion) REFERENCES inscripcion (id_inscripcion),
+	FOREIGN KEY (id_propuesta) REFERENCES propuesta (id_propuesta)
 );
 
 CREATE TABLE configuracion (
@@ -20,7 +23,10 @@ CREATE TABLE jugador (
 	id_jugador,
 	id_infraccion,
 	id_calif,
-	handicap
+	handicap,
+	FOREIGN KEY (id_jugador) REFERENCES jugador (id_jugador),
+	FOREIGN KEY (id_infraccion) REFERENCES infraccion (id_infraccion),
+	FOREIGN KEY (id_calif) REFERENCES calificacion (id_calif)
 );
 
 CREATE TABLE calificacion (
@@ -28,7 +34,9 @@ CREATE TABLE calificacion (
 	puntaje,
 	descripcion,
 	id_jugador,
-	id_partido
+	id_partido,
+	FOREIGN KEY (id_jugador) REFERENCES jugador (id_jugador),
+	FOREIGN KEY (id_partido) REFERENCES partido (id_partido)
 );
 
 CREATE TABLE infraccion (
@@ -43,11 +51,13 @@ CREATE TABLE propuesta (
 	fecha,
 	fecha_rechazo,
 	motivo_rechazo,
-	estado
+	estado,
+	FOREIGN KEY (id_jugador) REFERENCES jugador (id_jugador)
 );
 
 CREATE TABLE inscripcion (
 	id_inscripcion PRIMARY KEY,
 	id_jugador,
-	tipo
+	tipo,
+	FOREIGN KEY (id_jugador) REFERENCES jugador (id_jugador)
 );
