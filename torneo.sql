4fd880839b1169ca650ab46d96844f4445ae5055
Juan Pablo Jacob <jacobjuampe@hotmail.com>
2014-10-17 20:13:34 -0300

    Agrego set de datos al script

--- torneo.sql
+++ torneo.sql
@@ -58,12 +58,12 @@ create table if not exists infracciones (
 create table if not exists propuestas (
 	cod_propuesta int primary key auto_increment,
 	jugador int not null, /* jugador que hace la propuesta */
-    jugador_propuesto int not null,
+	jugador_propuesto int not null,
 	fecha_formulacion date not null,
 	fecha_rechazo date check (fecha_rechazo > fecha_formulacion),
 	motivo_rechazo varchar(50),
-	estado varchar(50), /* state */
-    foreign key (jugador) references jugadores (cod_jugador),
+	estado varchar(50), /* state - ACEPTADA / PENDIENTE / RECHAZADA */
+	foreign key (jugador) references jugadores (cod_jugador),
 	foreign key (jugador_propuesto) references jugadores (cod_jugador)
 );
 
@@ -76,36 +76,75 @@ create table if not exists inscripciones (
 	foreign key (partido) references partidos (cod_partido)
 );
 
-
 insert into ordenadores (descripcion)
 values
 	('Handicap'),
 	('Promedio del Ultimo Partido'),
-    ('Promedio de las Ultimas Calificaciones');
+	('Promedio de las Ultimas Calificaciones');
 
 insert into armadores (descripcion)
 values
 	('Posiciones Pares e Impares'),
-    ('Posiciones Preestablecidas');
+	('Posiciones Preestablecidas');
 
 insert into configuraciones (ordenador, armador)
 values (1, 1);
 
 insert into partidos (fecha, lugar, configuracion)
-values ('2014-10-10', 'Pilar', 1);
+values
+	('2014-8-10', 'Pilar', 1),
+	('2014-9-23', 'Puerto Madero', 1);
 
 insert into jugadores (nombre, edad, fecha_nacimiento, amigo)
 values
 	('Gaston', 28, '1986-10-10', null),
-    ('Franco', 26, '1988-10-10', null),
-    ('Matias', 25, '1989-10-10', null),
-    ('Ariel', 25, '1989-10-10', null),
-    ('Pollo', 27, '1987-10-10', null),
-    ('Nicolas', 33, '1981-10-10', null),
-    ('Demian', 31, '1983-10-10', null),
-    ('Pablo', 28, '1986-10-10', null),
-    ('Ernesto', 27, '1987-10-10', null),
-    ('Federico', 26, '1988-10-10', null);
+	('Franco', 26, '1988-10-10', null),
+	('Matias', 25, '1989-10-10', null),
+	('Ariel', 25, '1989-10-10', null),
+	('Pollo', 27, '1987-10-10', null),
+	('Nicolas', 33, '1981-10-10', null),
+	('Demian', 31, '1983-10-10', null),
+	('Pablo', 28, '1986-10-10', null),
+	('Ernesto', 27, '1987-10-10', null),
+	('Federico', 26, '1988-10-10', null),
+	('Juan', 21, '1993-10-10', null),
+	('Agustin', 21, '1993-10-10', null),
+	('Manuel', 21, '1993-10-10', null),
+	('Esteban', 21, '1993-10-10', null),
+	('Lucas', 30, '1984-10-10', null);
+
+insert into inscripciones (jugador, partido, tipo)
+values
+	(1, 1, 'Estandar'),
+	(2, 1, 'Estandar'),
+	(3, 1, 'Estandar'),
+	(4, 1, 'Estandar'),
+	(5, 1, 'Estandar'),
+	(6, 1, 'Estandar'),
+	(7, 1, 'Estandar'),
+	(8, 1, 'Estandar'),
+	(9, 1, 'Estandar'),
+	(10, 1, 'Estandar'),
+	(11, 1, 'Estandar'),
+	(12, 1, 'Estandar'),
+	(13, 1, 'Estandar'),
+	(14, 1, 'Estandar'),
+	(15, 1, 'Estandar');
+
+insert into infracciones (jugador, fecha, motivo)
+values
+	(7, '2014-10-10', 'Llego tarde al partido'),
+	(2, '2014-10-15', 'Tarjeta amarilla'),
+	(3, '2014-10-14', 'Escupio a un jugador'),
+	(4, '2014-11-22', 'Llego a los 20 fouls'),
+	(5, '2014-11-23', 'Tarjeta roja'),
+	(6, '2014-12-12', 'Golpeo al arbitro');
+
+insert into propuestas (jugador, jugador_propuesto, fecha_formulacion)
+values
+	(1, 3, '2014-9-4'),
+	(1, 5, '2014-9-4'),
+	(4, 7, '2014-9-4');
 
 /*
 drop table if exists calificaciones;
